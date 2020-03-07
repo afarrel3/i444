6ec18d182548f5b641612604847d47d3d242610f
Austin M Farrell <afarrel3@binghamton.edu>
2020-03-07 15:40:57 -0500

    Added given files to project

---
+++ submit/prj2-sol/blog544.js
@@ -0,0 +1,123 @@
+// -*- mode: JavaScript; -*-
+
+import mongo from 'mongodb';
+
+import BlogError from './blog-error.js';
+import Validator from './validator.js';
+import assert from 'assert';
+
+//debugger; //uncomment to force loading into chrome debugger
+
+/**
+A blog contains users, articles and comments.  Each user can have
+multiple Role's from [ 'admin', 'author', 'commenter' ]. An author can
+create/update/remove articles.  A commenter can comment on a specific
+article.
+
+Errors
+======
+
+DB:
+  Database error
+
+BAD_CATEGORY:
+  Category is not one of 'articles', 'comments', 'users'.
+
+BAD_FIELD:
+  An object contains an unknown field name or a forbidden field.
+
+BAD_FIELD_VALUE:
+  The value of a field does not meet its specs.
+
+BAD_ID:
+  Object not found for specified id for update/remove
+  Object being removed is referenced by another category.
+  Other category object being referenced does not exist (for example,
+  authorId in an article refers to a non-existent user).
+
+EXISTS:
+  An object being created already exists with the same id.
+
+MISSING_FIELD:
+  The value of a required field is not specified.
+
+*/
+
+export default class Blog544 {
+
+  constructor(meta, options, data) {
+    this.meta = meta;
+    this.options = options;
+    this.data = data;
+    this.validator = new Validator(meta);
+  }
+
+  /** options.dbUrl contains URL for mongo database */
+  static async make(meta, options) {
+    const data = Data.make(meta, options);
+    return new Blog544(meta, options, data);
+  }
+
+  /** Release all resources held by this blog.  Specifically, close
+   *  any database connections.
+   */
+  async close() {
+    //@TODO
+
+    //Close mongo DB connection
+
+
+  }
+
+  /** Remove all data for this blog */
+  async clear() {
+    this.data = Data.make(this.meta, this.options);
+  }
+
+  /** Create a blog object as per createSpecs and 
+   * return id of newly created object 
+   */
+  async create(category, createSpecs) {
+    const obj = this.validator.validate(category, 'create', createSpecs);
+    return this.data.create(category, obj);
+  }
+
+  /** Find blog objects from category which meets findSpec.  
+   *
+   *  First returned result will be at offset findSpec._index (default
+   *  0) within all the results which meet findSpec.  Returns list
+   *  containing up to findSpecs._count (default DEFAULT_COUNT)
+   *  matching objects (empty list if no matching objects).  _count .
+   *  
+   *  The _index and _count specs allow paging through results:  For
+   *  example, to page through results 10 at a time:
+   *    find() 1: _index 0, _count 10
+   *    find() 2: _index 10, _count 10
+   *    find() 3: _index 20, _count 10
+   *    ...
+   *  
+   */
+  async find(category, findSpecs={}) {
+    const obj = this.validator.validate(category, 'find', findSpecs);
+    return this.data.find(category, obj);
+  }
+
+  /** Remove up to one blog object from category with id == rmSpecs.id. */
+  async remove(category, rmSpecs) {
+    const obj = this.validator.validate(category, 'remove', rmSpecs);
+    this.data.remove(category, obj);
+  }
+
+  /** Update blog object updateSpecs.id from category as per
+   *  updateSpecs.
+   */
+  async update(category, updateSpecs) {
+    const obj = this.validator.validate(category, 'update', updateSpecs);
+    this.data.update(category, obj);
+  }
+  
+}
+
+const DEFAULT_COUNT = 5;
+
+const MONGO_CONNECT_OPTIONS = { useUnifiedTopology: true };
---
+++ submit/prj2-sol/validator.js
@@ -0,0 +1,201 @@
+// -*- mode: JavaScript; -*-
+
+import BlogError from './blog-error.js';
+
+export default class Validator {
+
+  constructor(meta) {
+    const xInfo = {};   //Object that holds the meta information
+    const actions = {}; //Object that holds 
+
+    //For each key-value pair in the meta object
+    //Categories are users, articles, and comments
+    for (const [category, infos] of Object.entries(meta)) {
+      
+      //Get the current category's fields, as field-name - object pairs
+      //i.e. id: {name: 'id', friendlyName: 'user ID', ...}
+      xInfo[category] = Object.fromEntries(infos.map(info => [info.name, info]));
+
+      //Get the list of actions, and their number of fields that are 'required'
+      //'forbidden', and 'optional', for the current category (users...)
+      actions[category] = makeActions(infos);
+    }
+
+    //Save the meta category information
+    this.meta = xInfo;
+
+    //Save the meta action information
+    this.actionFields = actions;
+  }
+
+  validate(category, act, obj) {
+    //Declare new object to return for the given category and action
+    //i.e. create(), find()...
+    const out = {};
+
+    //Get the information for the given category
+    const infos = this.meta[category];
+      
+    //Determine whether an invalid category was given
+    //i.e. not 'users', 'articles', 'comments'
+    if (!infos) {
+      throw [ new BlogError(`BAD_CATEGORY', 'unknown category ${category}`) ];
+    }
+
+    //Create an array to hold errors
+    const errors = [];
+
+    //Get the required fields for the given category and action
+    const required = new Set(this.actionFields[category][act].required);
+
+    //Get the 'forbidden' and 'optional' fields for the given category and action
+    const { forbidden, optional } = this.actionFields[category][act];
+
+    //Build an error message suffix for the given category and action
+    const msgSuffix = `for ${category} ${act}`;
+
+    //For 
+    for (const [name, value] of Object.entries(obj)) {
+      
+      required.delete(name);
+
+      const info = infos[name];
+      //
+      if (name.startsWith('_')) {
+        out[name] = value;
+      }
+      //Determine whether the current field has no info
+      else if (info === undefined) {
+	      const msg = `unknown ${category} field ${name} ${msgSuffix}`;
+	      errors.push(new BlogError('BAD_FIELD', msg));
+      }
+      //Determine whether the current field belongs to the 'forbidden' list
+      else if (forbidden.has(name)) {
+      	const msg = `the ${info.friendlyName} field is forbidden ${msgSuffix}`;
+	      errors.push(new BlogError('BAD_FIELD', msg));
+      }
+      //Determine whether the current field has a 'checkFn' attribute, and the
+      //value of the attribute doesn't meet the checkFn requirements
+      else if (info.checkFn && !info.checkFn(value)) {
+	      const msg = `bad value: ${value}; ${info.checkError} ${msgSuffix}`;
+	      errors.push(new BlogError('BAD_FIELD_VALUE', msg));
+      }
+      else {
+        //Add the field's name and attribute to the return object
+        out[name] = info.data ? info.data(value) : value;
+      }
+    } //for
+
+    //Determine whether there are still required items with info, but
+    //missing names
+    if (required.size > 0) {
+      //Map the fields' friendly names to the new array, since their true names
+      //are missing
+      const names = Array.from(required).
+	      map(n => infos[n].friendlyName).
+        join(', ');
+        
+      errors.push(new BlogError('MISSING_FIELD',
+				`missing ${names} fields ${msgSuffix}`));
+    }
+
+    //Throw the list of errors
+    if (errors.length > 0) throw errors;
+
+    //For each optional field
+    //Fill in default value for optional fields
+    for (const name of optional) {
+      //Determine whether the current optional field is not in the return
+      //objject, and the field has a default function
+      if (out[name] === undefined && infos[name].defaultFn !== undefined) {
+        //Add the optional field to the return object, givin it a value using
+        //the default function
+	      out[name] = infos[name].defaultFn();
+      }
+    }
+    
+    //Return object is an object containing og the given category resulting
+    //from the given action
+    //i.e. 'comments' object resulting from 'create' 
+    return out;
+  }
+};
+
+function makeActions(infos) {
+  //Possible actions for the user to take
+  const acts = [ 'create', 'find', 'update', 'remove', ];
+
+  //Create array of each action in 'acts', followed by the number of fields 
+  //required and forbidden for those actions
+  const pairs = acts.map(a => [a, { required: new Set(), forbidden: new Set(), }]);
+
+  //Transform the 'pairs' key-value pairs array into an object
+  const actions = Object.fromEntries(pairs);
+
+  //For each field map in the category sequence
+  //i.e., the group of objects under 'users', 'articles'... in the meta
+  for (const info of infos) {
+
+    //Get the name of the current field map (id, email, firstname, authorId...)
+    const name = info.name;
+
+    //For each action availability in the meta (fields can either be
+    //'required' or 'forbidden' for certain actions)
+    for (const k of [ 'required', 'forbidden' ]) {
+      
+      //For each action in the list of available actions for the current
+      //action availability for the current category field
+      //i.e. 'create' from [ 'create' ] for 'required' for 'email' for 'users'
+      //then 'remove' from [ 'remove' ] for 'forbidden' for 'email' for 'users'
+      for (const act of info[k] || []) {
+
+        //Add the current field name to the list of fields that are 'required' or
+        //'forbidden' for a given action for the current category
+        //
+        //i.e. adding 'firstName' to the 'required' list for the 'create' action
+        //in the 'users' category
+    	  actions[act][k].add(name);
+      }
+    }
+  }
+
+  //Get an array for each category's fields' 'name' attribute
+  //i.e. for 'users', [ 'id', 'email', 'firstName',... ]
+  const names = infos.map(info => info.name);
+
+  //For each action in the list of actions
+  //i.e. 'create' from [ 'create', 'find', 'update', 'remove' ]
+  for (const act of acts) {
+
+    //Get the objects that associate how many fields are 'required' and
+    //'forbidden' for each action for each category
+    //i.e. for 'create' for 'users' 
+    //  required: Set(5) { 'id', 'email', 'firstName', 'lastName', 'roles' },
+    //  forbidden: Set(0) {}
+    const actInfo = actions[act];
+    
+    //Add a new 'optional' action availability set
+    actInfo.optional = new Set();
+
+    //For each field name in the array of each category's fields' 'name'
+    //attributes
+    for (const name of names) {
+      
+      //If the current field name is not 'required' nor 'forbidden'
+      if (!actInfo.required.has(name) && !actInfo.forbidden.has(name)) {
+        //Add the current field name to the 'optional' set
+	      actInfo.optional.add(name);
+      }
+    }
+  }
+  
+  //Return the object containing how many fields are 'required', 'forbidden' and
+  //'optional' for each action for each category
+  return actions;
+}
+
+/*
+import util from 'util';
+import meta from './meta.mjs';
+console.log(util.inspect(new Validator(meta), false, null));
+*/
